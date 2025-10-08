ab318537d70445f86a8afdd6a987590b1ab0a788
Tim Juestel <tim.juestel@stud.hs-merseburg.de>
2025-10-08 16:29:55 +0200

    lol

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,13 @@ public class HelloWorld {
 public static void main(String[] args){
 
 System.out.println("Hello World!");
+int counter = 10;
+
+if(counter == 10){
+    System.out.println("Lol!");
+}else{
+    System.out.println("Nicht 10!");
+}
 
 
 }
